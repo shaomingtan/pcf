8735e887c514530f43336b9a1193fca6a6da704e
Shao Tan <shaoming.tan@circles.asia>
2021-02-18 13:05:07 +0800

    Initial implementation of Nchf_spendinglimitcontrol_subscribe invocation logic in smf Add configuration for PCF to determine if there's a need to interface with CHF as well as load custom session rules

--- producer/smpolicy.go
+++ producer/smpolicy.go
@@ -15,7 +15,9 @@ import (
 	"github.com/free5gc/openapi"
 	"github.com/free5gc/openapi/Nudr_DataRepository"
 	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/pcf/chf_util"
 	pcf_context "github.com/free5gc/pcf/context"
+	"github.com/free5gc/pcf/factory"
 	"github.com/free5gc/pcf/logger"
 	"github.com/free5gc/pcf/util"
 )
@@ -134,6 +136,26 @@ func createSMPolicyProcedure(request models.SmPolicyContextData) (
 			PriorityLevel: defQos.PriorityLevel,
 			// AverWindow
 			// MaxDataBurstVol
+		}
+	}
+	// TODO: Figure out where in the code to properly locate this logic
+	// It states in TS 129 513 Section 5.3.2 that `The PCF retrieves subscription information that indicates that policy decisions depend on policy counter(s) held` to determine if it should Invoke Nchf_spendinglimitControl_subscribe.
+	// We need to determine what attribute in the subscription info is used to signal this.
+	// 1 potential clue could be in the SessionManagementPolicyData that is retrived from UDR but I can't seem to find much information so far. see TS 129 519 section 5.2.5
+	// Another potential clue could be in the smPolicyContextData either provided by SMF or if not present, configured in the PCF as it includes a bunch of attributes relating to charging. See TS 129 512 Section 4.2.2.3.2
+	// Nchf_SpendingLimitControl_subscribe request if config queryCHFSpendingLimit is turned on
+	queryCHFSpendingLimit := factory.PcfConfig.Configuration.QueryCHFSpendingLimit
+	if queryCHFSpendingLimit {
+		// Nchf_SpendingLimitControl subscribe and retrieve policyCounters
+		spendingLimitStatus, problemDetail := chf_util.SubscribeSpendingLimit(ue.Supi, ue.Gpsi)
+
+		if problemDetail != nil {
+			return nil, nil, problemDetail
+		}
+
+		// Make policy decisions
+		if spendingLimitStatus != nil {
+			chf_util.MakeSpendingLimitDecision(spendingLimitStatus, &sessRule)
 		}
 	}
 	decision.SessRules[SessRuleId] = &sessRule
